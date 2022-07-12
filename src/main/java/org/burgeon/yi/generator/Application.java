7687ccfa6b986b8c5af1219f66f12036e00d457e
Sam Lu <luxiaocong524@163.com>
2022-07-12 08:34:34 +0800

    refactor(1.0.0): 更改包名 org.burgeon 为 org.bg181

--- src/main/java/org/burgeon/yi/generator/Application.java
+++ src/main/java/org/burgeon/yi/generator/Application.java
@@ -1,4 +1,4 @@
-package org.burgeon.yi.generator;
+package org.bg181.yi.generator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
